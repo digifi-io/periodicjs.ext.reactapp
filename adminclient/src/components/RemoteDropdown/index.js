394128b5895d92978595b0ed257d9f14538fd624
iris <yjdan92@gmail.com>
2018-06-28 12:02:46 -0400

    before making it configurable

--- adminclient/src/components/RemoteDropdown/index.js
+++ adminclient/src/components/RemoteDropdown/index.js
@@ -23,59 +23,54 @@ class RemoteDropdown extends Component {
 
   handleChange = (e, { value }) => this.setState({ value })
 
-  debounce(func, wait, immediate, searchQuery) {
+  debounce(func, wait, immediate) {
     var timeout;
     const self = this;
-    var context = self, args = arguments;
-    var later = function () {
-      timeout = null;
-      console.log('PREVIOUS', self.state.searchQuery);
-      console.log('CURRENT', searchQuery);
-      if (!immediate && self.state.searchQuery !== searchQuery) {
-        console.log('DIFF')
-        self.setState({ searchQuery }, () => {
-          func.apply(context, args);
-        });
-      } else {
-        console.log('SAME');
-        self.setState({ isFetching: false });
-      }
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+    return function () {
+      var context = self, args = arguments;
+      var later = function () {
+        timeout = null;
+        func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    }
   }
-
-  handleSearchChange = (e, { searchQuery }) => this.debounce(() => {
-    this.setState({ isFetching: true })
+  handleSearchChange = this.debounce((e, { searchQuery }) => {
     const self = this;
-    let stateProps = self.props.getState();
-    let options = self.props.searchProps;
-    let fetchURL = `${stateProps.settings.basename}${options.baseUrl}&${qs.stringify({
-      limit: self.state.limit || self.props.limit || 50,
-      sort: '-createdat',
-      query: searchQuery,
-      allowSpecialCharacters: true,
-      pagenum: options.pagenum || 1,
-    })}`;
-    let headers = Object.assign({
-      'x-access-token': stateProps.user.jwt_token,
-    }, stateProps.settings.userprofile.options.headers);
-    utilities.fetchComponent(fetchURL, { headers, })()
-      .then(response => {
-        console.log({ response });
-        let dropdown = response[ 'test_search' ].map(item => ({
-            "key": item.value,
-            "text": item.label,
-            "value": item.value,
-        }));
-        self.setState({ isFetching: false, options: dropdown })
-      }, e => {
-        self.setState({ isFetching: false, options: [] })
-        console.log({ e })
-      });
-  }, 3000, false, searchQuery)
+    if (searchQuery && self.state.searchQuery !== searchQuery) {
+      self.setState({ searchQuery }, () => {
+        self.setState({ isFetching: true })
+        let stateProps = self.props.getState();
+        let options = self.props.searchProps;
+        let fetchURL = `${stateProps.settings.basename}${options.baseUrl}&${qs.stringify({
+          limit: self.state.limit || self.props.limit || 50,
+          sort: '-createdat',
+          query: searchQuery,
+          allowSpecialCharacters: true,
+          pagenum: options.pagenum || 1,
+        })}`;
+        let headers = Object.assign({
+          'x-access-token': stateProps.user.jwt_token,
+        }, stateProps.settings.userprofile.options.headers);
+        utilities.fetchComponent(fetchURL, { headers, })()
+          .then(response => {
+            let dropdown = response[ 'test_search' ].map((item, idx) => ({
+              "key": idx,
+              "text": item.label,
+              "value": item.value,
+            }));
+            self.setState({ isFetching: false, options: dropdown })
+          }, e => {
+            self.setState({ isFetching: false, options: [] })
+          });
+      })
+    } else {
+      self.setState({ isFetching: false });
+    }
+  }, 1000, false)
 
   toggleSearch = e => this.setState({ search: e.target.checked })
 
